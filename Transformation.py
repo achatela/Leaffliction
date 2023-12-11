ebb89d645ff3e93e26c632e5960945c4f5aa8a86
Alexis CHATELAIN <achatela@made-f0Ar8s4.clusters.42paris.fr>
2023-12-11 18:05:58 +0100

    changed Transformation to be a class

--- Transformation.py
+++ Transformation.py
@@ -6,57 +6,53 @@ from plantcv import plantcv as pcv
 import cv2
 import numpy as np
 
-def change_contrast(img, level):
-    factor = (259 * (level + 255)) / (255 * (259 - level))
-    def contrast(c):
-        value = 128 + factor * (c - 128)
-        return max(0, min(255, value))
-    return img.point(contrast)
 
-def gaussian_blur(path):    
-    image = cv2.imread(path, 0)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gaussian_blur = cv2.GaussianBlur(gray_image, (5, 5), sigmaX=0)
-    pcv.plot_image(gaussian_blur)
+class Transformation:
 
+    def __init__(self, path_to_file):
+        self.path_type = self.check_path(path_to_file)
+        self.original = self.open_original(path_to_file)
+        pcv.plot_image(self.original)
+        if self.path_type == 1:
+            self.image_transformation()
 
-def canny_edges(path):
-    image = cv2.imread(path, 0)
-    gaussian_img = pcv.gaussian_blur(img=image, ksize=(5, 5), sigma_x=1, sigma_y=1)
-    edges = cv2.Canny(gaussian_img, threshold1=15, threshold2=90, L2gradient=False)
-    Hori = np.concatenate((image, gaussian_img, edges), axis=1)
-    
-    cv2.imshow('HORIZONTAL', Hori)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return ""
+    def image_transformation(self):
+        self.get_mask(self.original)
 
-def get_mask(path):
-    img = cv2.imread(path, 0)
-    device = 50
-    lp_img = pcv.laplace_filter(img, 1, device)
-    # mask, masked_img = pcv.threshold.custom_range(img=img, lower_thresh=[100], upper_thresh=[160], channel='gray')
-    pcv.plot_image(lp_img)
+    def check_path(self, path_to_file):
+        if os.path.isdir(path_to_file):
+            return 0
+        try:
+            Image.open(path_to_file)
+            return 1
+        except:
+            return 2
 
-def image_transformation(path):
-    # leaf_mask(path)
-    # gaussian_blur(path)
-    canny_edges(path)
-    get_mask(path)
+    def open_original(self, path_to_file):
+        return cv2.imread(path_to_file)
 
-def check_path(path):
-    if os.path.isdir(path):
-        return 0
-    try:
-        Image.open(path)
-        return 1
-    except:
-        return 2
+    def gaussian_blur(self, path):    
+        image = cv2.imread(path, 0)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gaussian_blur = cv2.GaussianBlur(gray_image, (5, 5), sigmaX=0)
+        pcv.plot_image(gaussian_blur)
+
+    def canny_edges(self, path):
+        image = cv2.imread(path, 0)
+        gaussian_img = pcv.gaussian_blur(img=image, ksize=(5, 5), sigma_x=1, sigma_y=1)
+        edges = cv2.Canny(gaussian_img, threshold1=15, threshold2=90, L2gradient=False)
+        Hori = np.concatenate((image, gaussian_img, edges), axis=1)
+        
+        cv2.imshow('HORIZONTAL', Hori)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return ""
+
+    def get_mask(self, image):
+        pass
 
 def main():
-    path_type = check_path(sys.argv[1])
-    if path_type == 1:
-        image_transformation(sys.argv[1])
+    image = Transformation(sys.argv[1])
 
 if __name__ == "__main__":
     main()
