8421ce965fb175a94481205ac31ec5279840cb82
Alexis CHATELAIN <achatela@paul-f4Ar7s7.clusters.42paris.fr>
2024-01-18 17:48:10 +0100

    train.py norme

--- train.py
+++ train.py
@@ -5,20 +5,25 @@ import sys
 import os
 import random
 import shutil
-import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def create_model(input_shape, num_classes):
-    model = models.Sequential()
-    # Conv2d explication : https://towardsdatascience.com/conv2d-to-finally-understand-what-happens-in-the-forward-pass-1bbaafb0b148
-    # MaxPooling2D schema : https://media.geeksforgeeks.org/wp-content/uploads/Screenshot-from-2017-08-15-17-04-02.png
-    # Flatten rend l'image en 1D : https://i.stack.imgur.com/rx3X4.png
-    # Dense reseau de neurone tous connectes entre eux : https://i.stack.imgur.com/Gdpz7.png
-    # Dropout cree un nouveau layer avec des neurones qui ont une probabilité de 0.5 d'être désactivés
-    # relu = fonction qui change les nombres negatifs en 0
 
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+def create_model(input_shape, num_classes):
+
+    model = models.Sequential()
+    # Conv2d explication :
+    # https://towardsdatascience.com/conv2d-to-finally-understand-what-happens-in-the-forward-pass-1bbaafb0b148
+    # MaxPooling2D schema :
+    # https://media.geeksforgeeks.org/wp-content/uploads/Screenshot-from-2017-08-15-17-04-02.png
+    # Flatten rend l'image en 1D :
+    # https://i.stack.imgur.com/rx3X4.png
+    # Dense reseau de neurone tous connectes entre eux :
+    # https://i.stack.imgur.com/Gdpz7.png
+    # Dropout cree un nouveau layer avec 0.5 des neurones desactives :
+
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=input_shape))
     model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -38,10 +43,13 @@ def create_model(input_shape, num_classes):
 
     return model
 
-def train_model(train_data_dir, val_data_dir, batch_size, epochs, model_save_path, num_classes=8):
+
+def train_model(train_data_dir, val_data_dir, batch_size, epochs,
+                model_save_path, num_classes=8):
+
     # Image data generators for data augmentation
     train_datagen = ImageDataGenerator(rescale=1./255)
-    
+
     val_datagen = ImageDataGenerator(rescale=1./255)
 
     # Flow training images in batches using train_datagen generator
@@ -75,7 +83,6 @@ def train_model(train_data_dir, val_data_dir, batch_size, epochs, model_save_pat
 
 
 def dataset_augmentation(augmentations_needed):
-   # augmentations_needed: array of tuples: (images_path, number of images needed, number of images in the directory, directory_dest)
 
     image_created = 0
     for augmentation in augmentations_needed:
@@ -85,7 +92,7 @@ def dataset_augmentation(augmentations_needed):
             if not index_range:
                 break
             index = random.choice(index_range)
-            image_path = os.listdir(augmentation[0])[index - 1] # subtract 1 because list indices start at 0
+            image_path = os.listdir(augmentation[0])[index - 1]
             augment_images(augmentation[0] + '/' + image_path, augmentation[3])
             index_range.remove(index)
         print(f'{augmentation[0]}: {augmentation[1] * 6} images added')
@@ -110,30 +117,47 @@ def main():
             os.system(f'rm -rf {directory_dest}')
         if not os.path.exists(directory_dest):
             os.mkdir(directory_dest)
-            for image in os.listdir(os.path.join(sys.argv[1],augmentation[0])):
-                os.system(f'cp \"{sys.argv[1] + augmentation[0]}/{image}\" {directory_dest}')
-        image_in_dir = len(distribution.arborescence[augmentation[0].split('_')[0]][augmentation[0]])
-        augmentations_needed[augmentations_needed.index(augmentation)] = (sys.argv[1] + augmentation[0], augmentation[1] // 6, image_in_dir, directory_dest)
+            for image in os.listdir(os.path.join(sys.argv[1],
+                                                 augmentation[0])):
+                os.system(
+                    f'cp \"{sys.argv[1] + augmentation[0]}/{image}\" \
+                        {directory_dest}'
+                    )
+        image_in_dir = \
+            len(distribution.arborescence[augmentation[0]
+                                          .split('_')[0]][augmentation[0]])
+        augmentations_needed[augmentations_needed.index(augmentation)] = (
+            sys.argv[1] + augmentation[0],
+            augmentation[1] // 6,
+            image_in_dir,
+            directory_dest)
+
     for subdirectory in distribution.subdirectories:
         if subdirectory not in new_subdirectories:
-            os.system(f'cp -r \"{sys.argv[1] + subdirectory}\" ./train_augmented/')
+            os.system(
+                f'cp -r \"{sys.argv[1] + subdirectory}\" \
+                    ./train_augmented/'
+            )
     dataset_augmentation(augmentations_needed)
 
 
 if __name__ == '__main__':
-    train_data_directory = '../augmented_dataset/'  # Update with your augmented dataset directory
-    val_data_directory = '../images/'  # Update with your validation dataset directory
+    train_data_directory = '../augmented_dataset/'
+    val_data_directory = '../images/'
     batch_size = 320
     epochs = 10
     model_save_path = 'leaf_disease_model.h5'
     if len(sys.argv) < 2:
-        sys.exit('usage: python3 train.py <directory_path> <flag> -t for training, -a for augmentation')
+        sys.exit('usage: python3 train.py <directory_path> \
+                 <flag> -t for training, -a for augmentation')
     if sys.argv[1] == '--train':
-        train_model(train_data_directory, val_data_directory, batch_size, epochs, model_save_path)
+        train_model(train_data_directory,
+                    val_data_directory, batch_size, epochs, model_save_path)
     elif sys.argv[1] == '--zip':
         shutil.make_archive('dataset', 'zip', "./train_augmented/")
     elif sys.argv[2] == '--augment':
         main()
     elif sys.argv[2] == '--transform':
-        create_transformations(["", sys.argv[1], "./train_augmented/" + sys.argv[1].split('/')[-1]])
-        # shutil.make_archive('dataset', 'zip', "./train_augmented/")
+        create_transformations(["", sys.argv[1],
+                                "./train_augmented/" +
+                                sys.argv[1].split('/')[-1]])
